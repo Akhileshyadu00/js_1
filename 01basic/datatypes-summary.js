057f149bf91e7cfcf0ec13adb176d894e9f47397
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-17 16:04:23 +0000

    datatype-summary

---
+++ 01basic/datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitives
+  // 7 : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+// Non-Primitives  (Reference Type)
+ // Arrays, Objects, Functions
+
+let myobj = {
+    name : "akhilesh",
+    id : "1"
+}
+const Functions = function(){
+    console.log("hello")
+}
+Functions();
+
+
+
+
+const id = Symbol('123')
+const id1 = Symbol('123')
+
+console.log(id===id1);
+
