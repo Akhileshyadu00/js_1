a80ea228e025a38209d592d8f1596843a930b73b
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-17 15:50:08 +0000

    comaprison

--- 01basic/conversionOperation.js
+++ 01basic/conversionOperation.js
@@ -7,4 +7,20 @@
  console.log(booleanIsLoggenIn)
 
  // ""  false
+
+ console.log("1" + 2 + 2);  //122
+ console.log(1 + 2 + "2");  //32
+
+ console.log(+true);
+ console.log(+"");
+
+ //prefix & postfix
+ //If used postfix, with operator after operand (for example, x++), 
+ // the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x),
+//  the increment operator increments and returns the value after incrementing.
+ 
+
+ 
  
