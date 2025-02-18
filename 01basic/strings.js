4d9e7878c1bd7c26c2452d3adda55be39100ad3c
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-18 13:59:36 +0000

    strings2

--- 01basic/strings.js
+++ 01basic/strings.js
@@ -7,3 +7,35 @@ console.log(name + id);
 console.log(`hello, ${name}, and id is ${id}`);
 
 // --proto--
+
+// .toUpperCase()
+// .charAt(2). at index what character
+// .indexOf("h"). at which index character lies
+
+const newName = name.substring(1,4)
+console.log(newName);
+
+const secName = name.slice(0, 4)
+console.log(secName);
+
+const sec1Name = name.slice(-4, 4);  //not workk
+console.log(sec1Name);  // negative allowed
+
+
+const three = "   akhilesh  "  //remove start and end space--- white space and line termination
+console.log(three);
+console.log(three.trim());
+
+const url = "https://google.com/akhilesh%20yadav"
+console.log(url.replace("%20", "-"));
+
+//include -- if present true, else false
+
+
+//split
+
+console.log(name.split(" "));
+console.log(name.bold())
+
+
+
