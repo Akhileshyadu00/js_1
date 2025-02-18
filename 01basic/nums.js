783f2dc57ea8872dbd9d6ccbf11c6cd45d92e061
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-18 14:19:06 +0000

    nums

---
+++ 01basic/nums.js
@@ -0,0 +1,34 @@
+const balance  = new Number(100)
+//console.log(typeof balance);
+//console.log(balance);
+
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));  //after decimal, precision value. (100.00)
+
+
+const avg = 123.688887
+//console.log(avg.toPrecision(3)) //precise value --> before decimal also include like number
+
+const hundreds = 100000000
+//console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+///// -------- MAths ------- /////
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4444)); //4
+console.log(Math.ceil(4.4444));  //5
+console.log(Math.floor(4.4444));  //4
+
+console.log(Math.min(4, 5, 6, 7));
+console.log(Math.max(4, 5, 6, 7));
+console.log(Math.random());
+
+
+
+
+
+
