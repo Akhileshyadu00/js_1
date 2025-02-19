619bc974e0c759bfda0de0ecb5af61f750e2c26c
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-19 14:09:53 +0000

    obj2

---
+++ 02basic/object1.js
@@ -0,0 +1,14 @@
+const course  = {
+    name: "JS",
+    price: "999",
+    teacher: "MEEEE"
+}
+
+//course.teacher()
+
+const {teacher} = course
+//console.log(teacher);
+
+const navbar = () => {
+    
+}
