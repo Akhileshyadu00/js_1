e99a4509703309cb4a04a3f47ebb73de305a13dd
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-22 07:44:46 +0000

    loops

--- 03basic/loops.js
+++ 03basic/loops.js
@@ -5,11 +5,55 @@
 for (let i = 0; i < 10; i++) {
     const element = i;
     if(element == 5) {
-        console.log("5 is best number");
+       // console.log("5 is best number");
         
     }
-    console.log(element);
+    //console.log(element);
     
     
 }
 
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`outer loop ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`inner loop ${j}`);
+      // console.log(i + "*" + j + " , " + i*j);
+       
+        
+    }
+    
+}
+
+let heros = ["ironman", "thor", "antman"]
+
+for (let index = 0; index < heros.length; index++) {
+    const element = heros[index];
+    //console.log(element);
+    
+    
+}
+
+
+// break and continue
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        console.log(`5 detected`);
+        //console.log(`value of index is ${index}`);
+        break
+    }
+   console.log(`value of index is ${index}`); 
+}
+
+
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        console.log(`5 detected`);
+        //console.log(`value of index is ${index}`);
+       continue
+    }
+   console.log(`value of index is ${index}`); 
+}
