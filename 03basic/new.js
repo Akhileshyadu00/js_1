ea73b505a95bbe7b90e2d2009f9454747ca4030b
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-22 08:42:13 +0000

    foreach

--- 03basic/new.js
+++ 03basic/new.js
@@ -24,3 +24,52 @@ for (const key in map) {
     //console.log(key);  // map not itterable
     
 }
+
+
+// for each
+
+//callback - function inside function, called as parameter to another, no name to callback
+
+const coding = ["a", "b", "c", "d","e"]
+
+coding.forEach(function(item) {
+   // console.log(item);
+    
+}  )
+
+coding.forEach( (item) => {
+    //console.log(item);
+    
+} )
+
+function print(item) {
+   // console.log(item);
+    
+}
+coding.forEach(print)
+
+coding.forEach( (item, index, arr) =>  {
+    //console.log(item, index, arr);
+    
+} )
+
+const myCoding = [
+    {
+        name: "javascript",
+        file: "js"
+    },
+    {
+        name: "java",
+        file: "jv"
+    },
+    {
+        name: "react",
+        file: "rc"
+    }
+]
+
+myCoding.forEach( (item) => {
+
+    console.log(item.name);
+    
+} )
