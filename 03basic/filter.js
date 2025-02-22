f1aaefd1bd3630f88712a45b246611fd6f67c691
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-22 08:55:49 +0000

    filter

---
+++ 03basic/filter.js
@@ -0,0 +1,15 @@
+const coding = ["a", "b", "c", "d", "e"]
+
+const value = coding.forEach( (item) => {
+    //console.log(item);
+    return item  // not return value
+    
+})
+//console.log(value);  //undefined
+
+
+const myNUms = [1,2,3,4,5,6,7,8,9,]
+const newNums = myNUms.filter( (num) => num > 4)  // {} rertun empty array, use retun 
+console.log(newNums);
+
+
