71cf32f9afab85c1e4c88673818948b66c2d67ea
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-22 04:52:27 +0000

    execution

---
+++ 02basic/execution.js
@@ -0,0 +1,8 @@
+// Javascript Execution Context
+
+// {} Global Exectution Context(this) ----> Function Execution Context ---> Eval Execution Context
+
+//{ Memory Allocation Phase ---> Execution Phase}
+
+
+// CallStack  --> 
