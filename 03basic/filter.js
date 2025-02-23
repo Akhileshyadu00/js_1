e3387f8afe03852c59ff8f9695ea20c4b780cc51
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-02-23 04:15:16 +0000

    reduce

--- 03basic/filter.js
+++ 03basic/filter.js
@@ -10,6 +10,31 @@ const value = coding.forEach( (item) => {
 
 const myNUms = [1,2,3,4,5,6,7,8,9,]
 const newNums = myNUms.filter( (num) => num > 4)  // {} rertun empty array, use retun 
-console.log(newNums);
+//console.log(newNums);
+
+
+
+
+// 
+
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//const newNum = myNumber.map((num) => num + 10 )
+
+const newNum = myNumber
+        .map((num) => num * 10)
+        .map((num) => num + 1)
+        .filter((num) => num >= 40)
+
+//console.log(newNum);
+
+
+// Reduce
+
+const total = myNumber.reduce( (acc, cvalue) => acc + cvalue, 0) 
+console.log(total);
+
+
+
+
 
 
