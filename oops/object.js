eff8bb658df8a409131363542e3238d9070f9f52
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-03-01 05:58:26 +0000

    new

--- oops/object.js
+++ oops/object.js
@@ -8,3 +8,33 @@ console.log(multiply5(5));
 console.log(multiply5.power);
 console.log(multiply5.prototype);
 
+
+function createUser(userName, score) {
+    this.userName = userName;
+    this.score = score;
+}
+
+createUser.prototype.increament = function() {
+    this.score++;
+}
+
+createUser.prototype.print = function() {
+    console.log(`score is ${this.score}`);
+    
+}
+
+const chai = new createUser("Chai", 25);
+const tea =  new createUser("tea", 250);
+
+tea.print()
+
+//A new object is creted: new initaite the creation of new javascript object
+
+//A prtototypes is linked: new keywords provide constructor
+
+//Constructor is called
+
+//new object is returned
+
+
+
